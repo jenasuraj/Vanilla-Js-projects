e16f76e2eebb1b4f5cd8207c986bc8185aad8a36
jenasuraj <jenasuraj742@gmail.com>
2024-03-23 17:09:36 +0530

    done|

---
+++ Modal -Page/app.js
@@ -0,0 +1,34 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+/*const modalBtn=document.querySelector(".modal-btn");
+const modal=document.querySelector(".modal-overlay");
+const closebtn=document.querySelector(".close-btn");
+
+ modalBtn.addEventListener("click", function()
+ {
+modal.classList.add("open-modal");
+ });
+closebtn.addEventListener("click", function()
+ {
+    modal.classList.remove("open-modal");
+ });*/
+
+
+
+
+ const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlays');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click',function(){
+    modal.classList.add('open-modal');
+});
+closeBtn.addEventListener('click',function(){
+    modal.classList.remove('open-modal');
+});
+
+
+
+
